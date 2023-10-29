f4e48029ae436d64a42368bea22b7462a394d956
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-10-29 17:07:58 +0600

    orders inserted in database successfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -28,12 +28,31 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('carDoctorDB').collection('services')
+    const orderCollection = client.db('carDoctorDB').collection('orders')
 
     app.get('/services', async (req, res) => {
         const cursor = serviceCollection.find()
         const result = await cursor.toArray()
         res.send(result)
     })
+
+    app.get('/services/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const options = {
+            projection: { title: 1, price: 1, service_id: 1, img: 1 },
+        }
+        const result = await serviceCollection.findOne(query, options)
+        res.send(result)
+    })
+
+    // insert order to database
+    app.post('/orders', async (req, res) => {
+        const order = req.body;
+        const result = await orderCollection.insertOne(order)
+        res.send(result)
+    })
+    
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
